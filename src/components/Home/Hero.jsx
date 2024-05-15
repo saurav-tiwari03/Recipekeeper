534957f574a71a4f485ce542ca3d71d3d1bb95c1
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2024-05-16 02:28:13 +0530

    update

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,7 +1,44 @@
+import dosaImg from './../../assets/Dosa img.png'
+import samosaImg from './../../assets/Samosa img.png'
+import Typewriter from 'typewriter-effect';
+import {Link} from 'react-router-dom'
+
 
 export default function Hero() {
   return (
-    <div>This is Home Page</div>
+    <div className='flex flex-col md:flex-row justify-between'> 
+      {/*Right Side */}
+      <div className='flex items-center justify-center flex-col'>
+        <div className='flex items-center justify-center ml-10 flex-col'>
+          <h1 className="text-[#512C16] text-4xl max-w-[600px] font-bold">
+          Where cravings
+          <span className='text-[#d0c69f]'>
+            <Typewriter
+              options={{
+                strings: [' converge','meet','fulfill'],
+                autoStart: true,
+                loop: true,
+              }}
+              />
+          </span>
+          </h1>
+        </div>
+        <div className='flex items-start justify-start'>
+          <Link className='bg-[#f4eac4] text-xl font-semibold px-4 py-2 mt-4 rounded-xl drop-shadow transition-all duration-200'>Get Started</Link>
+        </div>
+      </div>
+      {/*Left Side */}
+      <div className=''>
+        <div>
+          <div className='flex flex-col bg-[#d0c69f] w-[600px] h-[600px] bg-glass'>
+            <img className='h-[300px] w-[325px]' src={samosaImg} alt="" />
+            <div className='flex items-end justify-end'>
+              <img className='h-[300px] w-[325px]' src={dosaImg} alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
   
