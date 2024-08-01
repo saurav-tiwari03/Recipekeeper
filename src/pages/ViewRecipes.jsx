db284b736bd20229dad3636d38fa0c4544e780af
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2024-08-02 00:46:47 +0530

    search user

--- src/pages/ViewRecipes.jsx
+++ src/pages/ViewRecipes.jsx
@@ -27,17 +27,18 @@ export default function ViewRecipes() {
       <div>
         <Navbar />
       </div>
-      <div className='flex items-center justify-center md:justify-start'>
+      <div className='flex flex-col md:flex-row items-center justify-center md:justify-between'>
         <SearchRecipe />
+        <SearchUser className='z-100'/>
       </div>
-      <div className='recipe-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 drop-shadow ml-4 mr-4'>
+      <div className='recipe-container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 ml-4 mr-4 mt-12'>
         {loading ? (
           <div className='flex items-center justify-center w-full h-64 absolute'>
             <div className="recipe-loader"></div>
           </div>
         ) : (
           recipes.map((recipe) => (
-            <div className='mx-8 m-4 md:mb-0 md:mx-0' key={recipe._id}>
+            <div className='mx-8 m-4 md:mb-0 md:mx-0 -z-100' key={recipe._id}>
               <div className='recipe-box overflow-y-hidden relative'>
                 <h1 className='text-3xl recipe-title absolute -translate-y-10 duration-300 font-Anton text-center w-full text-[#d1c79f] bg-[#744225]'>{recipe.title}</h1>
                 <img className='h-[250px] w-full recipe-image' src={recipe.imageUrl} alt="" />
@@ -65,5 +66,51 @@ function SearchRecipe() {
         <button className='rounded-lg py-2 px-4 bg-[#744225] text-white' onClick={searchHandler}>Search</button>
       </div>
     </div>
+  )
+}
+
+
+function SearchUser() {
+  const [input,setInput] = useState('');
+  const [allUsers, setAllUsers] = useState([]);
+  const searchUser = () => {
+    console.log(input);
+  }
+  useEffect(() => {
+    const getAllUser = async() => {
+      const res = await axios.get(`${import.meta.env.VITE_API_KEY}/user/allUsers`)
+      console.log(res.data.usernames);
+      setAllUsers(res.data.usernames);
+    }
+    getAllUser();
+  },[])
+  return (
+    <>
+    <div className='flex flex-col md:mr-4 relative '>
+      <div className='bg-[#d1c79f] p-4 flex gap-4 rounded-lg md:ml-4 justify-between'>
+        <input
+          className='bg-[transparent] outline-none border-b-2 border-gray-500 w-[250px] duration-300 focus:border-[#744224] placeholder:text-[#744225] placeholder:font-semibold placeholder:font-OpenSans'
+          type="text"
+          placeholder="Search user"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className='rounded-lg py-2 px-4 bg-[#744225] text-white' onClick={searchUser}>
+          Search
+        </button>
+      </div>
+      <div className='absolute top-14 z-200 drop-shadow w-[70%]'>
+        <ul className='bg-[#fff] ml-8 text-lg flex flex-col'>
+          {
+            input.trim() !== '' && allUsers
+              .filter(user => user.toLowerCase().includes(input.toLowerCase()))
+              .map(user => (
+                <Link to={`/user/${user}`} key={user}>{user}</Link>
+              ))
+          }
+        </ul>
+      </div>
+    </div>
+  </>
+  
   )
 }
